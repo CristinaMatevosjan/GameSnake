fbcb856585b0887e7a05bffdec3b0df29a6c3024
Kristina M <c.listeva@yandex.ru>
2023-04-23 22:53:28 +0300

    Добавьте файлы проекта.

---
+++ GameSnake/Program.cs
@@ -0,0 +1,75 @@
+﻿using System.Drawing;
+using System.Xml.Linq;
+
+namespace GameSnake
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+
+            //Console.SetBufferSize(80, 25);
+
+            Walls walls = new Walls(80, 25);
+            walls.Draw();
+
+            Point p = new Point(4, 5, '*');
+            Snake snake = new Snake(p, 4, Direction.RIGHT);
+            snake.Drow();
+
+            FoodCreator foodCreator = new FoodCreator(80, 25, '$');
+            Point food = foodCreator.CreateFood();
+            food.Draw();
+
+            while (true)
+            {
+                if (walls.IsHit(snake) || snake.IsHitTail())
+                {
+                    break;
+                }
+                if (snake.Eat(food))
+                {
+                    food = foodCreator.CreateFood();
+                    food.Draw();
+                }
+                else
+                {
+                    snake.Move();
+                }
+                Thread.Sleep(100);
+
+                if (Console.KeyAvailable)
+                {
+                    ConsoleKeyInfo keyInfo = Console.ReadKey();
+                    snake.HandleKey(keyInfo.Key);
+                }
+
+            }
+            WriteGameOver();
+            Console.ReadLine();
+
+        }
+
+        static void WriteGameOver()
+        {
+            int xOffset = 25;
+            int yOffset = 8;
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.SetCursorPosition(xOffset, yOffset++);
+            WriteText("============================", xOffset, yOffset++);
+            WriteText("И Г Р А    О К О Н Ч Е Н А", xOffset + 1, yOffset++);
+            yOffset++;
+            WriteText("Автор: Кристина Матевосян", xOffset + 2, yOffset++);
+            WriteText("============================", xOffset, yOffset++);
+        }
+
+        static void WriteText(String text, int xOffset, int yOffset)
+        {
+            Console.SetCursorPosition(xOffset, yOffset);
+            Console.WriteLine(text);
+        }
+
+    }
+}
+
+
---
+++ GameSnake/Snake.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameSnake
+{
+    class Snake : Figure
+    {
+
+        Direction direction1;
+
+        public Snake(Point tail, int length, Direction direction)
+        {
+
+            direction1 = direction;
+            pList = new List<Point>();
+
+            for (int i = 0; i < length; i++)
+            {
+                Point p = new Point(tail);
+                p.Move(i, direction);
+                pList.Add(p);
+            }
+        }
+
+        internal void Move()
+        {
+            Point tail = pList.First();
+            pList.Remove(tail);
+            Point head = GetNextPoint();
+            pList.Add(head);
+
+            tail.Clear();
+            head.Draw();
+
+        }
+
+        public Point GetNextPoint()
+        {
+            Point head = pList.Last();
+            Point nextPoint = new Point(head);
+            nextPoint.Move(1, direction1);
+            return nextPoint;
+        }
+
+        public void HandleKey(ConsoleKey key)
+        {
+            switch (key)
+            {
+                case ConsoleKey.LeftArrow:
+                    direction1 = Direction.LEFT;
+                    break;
+                case ConsoleKey.RightArrow:
+                    direction1 = Direction.RIGHT;
+                    break;
+                case ConsoleKey.DownArrow:
+                    direction1 = Direction.DOWN;
+                    break;
+                case ConsoleKey.UpArrow:
+                    direction1 = Direction.UP;
+                    break;
+            }
+        }
+
+        internal bool Eat(Point food)
+        {
+            Point head = GetNextPoint();
+            if (head.IsHit(food))
+            {
+                food.sym = head.sym;
+                pList.Add(food);
+                return true;
+            }
+            else return false;
+
+        }
+
+        internal bool IsHitTail()
+        {
+            var head = pList.Last();
+            for (int i = 0; i < pList.Count - 2; i++)
+            {
+                if (head.IsHit(pList[i]))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+    
+    }
+}
